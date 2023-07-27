f702b94c4c7d6043b43795d8e9c480be5725c33d
SangKyun-Seo <tkdrbs0802@naver.com>
2023-07-27 16:33:19 +0900

    feat: 아이템 정보 및 사이즈 정보 관련 mapper 구현

--- Grinn/src/main/java/kr/spring/trade/service/TradeService.java
+++ Grinn/src/main/java/kr/spring/trade/service/TradeService.java
@@ -1,5 +1,11 @@
 package kr.spring.trade.service;
 
+import java.util.List;
+
+import kr.spring.itemsize.vo.ItemSizeVO;
+
 public interface TradeService {
 
+	// 아이템 정보와 사이즈 정보 가져오기
+	public List<ItemSizeVO> selectGetItemSize(int item_num);
 }
