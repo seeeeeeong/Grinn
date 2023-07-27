6e1a88aa747e5a3fa3e7bafc6458a32f1b0e8bab
yijunesok <yijunesok@localhost>
2023-07-27 14:46:54 +0900

    UPDATE : style, report 테이블 생성

--- Grinn/src/main/webapp/sql/yjs.sql
+++ Grinn/src/main/webapp/sql/yjs.sql
@@ -24,8 +24,8 @@ CREATE sequence style_seq;
 --스타일 좋아요
 CREATE TABLE style_fav(
 	stfav_num number,
-	mem_num number,
-	st_num number,
+	mem_num number not null,
+	st_num number not null,
 	constraint style_fav_pk primary key (stfav_num),
 	constraint style_fk1 foreign key (mem_num) references member (mem_num) ON DELETE CASCADE,
     constraint style_fk2 foreign key (st_num) references style (st_num) ON DELETE CASCADE
@@ -39,8 +39,8 @@ CREATE TABLE style_comment(
 	comment varchar2(400) not null,
 	com_regdate DATE DEFAULT SYSDATE not null,
 	com_mdate DATE,
-	mem_num number,
-	st_num number,
+	mem_num number not null,
+	st_num number not null,
 	constraint style_comment_pk primary key (com_num),
 	constraint style_comment_fk1 foreign key (mem_num) references member (mem_num) ON DELETE CASCADE,
 	constraint style_comment_fk2 foreign key (st_num) references style (st_num) ON DELETE CASCADE
@@ -51,8 +51,8 @@ CREATE sequence style_comment_seq;
 --스타일 해시태그 연결
 CREATE TABLE style_tag_map(
 	map_num number,
-	tag_num number,
-	st_num number,
+	tag_num number not null,
+	st_num number not null,
 	constraint style_tag_map_pk primary key (map_num),
 	constraint style_tag_map_fk1 foreign key (tag_num) references style_tag (tag_num) ON DELETE CASCADE,
 	constraint style_tag_map_fk2 foreign key (st_num) references style (st_num) ON DELETE CASCADE
@@ -67,3 +67,44 @@ CREATE TABLE style_tag(
 	tag_count number DEFAULT 1 not null,
 	constraint style_tag_pk primary key (tag_num)
 );
+
+CREATE sequence style_tag_seq;
+
+--게시물 신고
+CREATE TABLE report_st(
+	rst_num number,
+	rep_type numer(1) not null,
+	rep_com varchar2(800),
+	rep_hide number(1) default 0 not null,
+	rep_status number(1) default 0 not null,
+	rep_regdate date default sysdate not null,
+	st_num number not null,
+	report_mem number not null,
+	reported_mem number not null,
+	constraint report_st_pk primary key (rst_num),
+	constraint report_st_fk1 foreign key (st_num) references style (st_num) ON DELETE CASCADE,
+	constraint report_st_fk2 foreign key (report_mem) references member (mem_num) ON DELETE CASCADE,
+	constraint report_st_fk3 foreign key (reported_mem) references member (mem_num) ON DELETE CASCADE
+	
+);
+
+CREATE sequence report_st_seq;
+
+--댓글 신고
+CREATE TABLE report_com(
+	rcom_num number,
+	rep_type numer(1) not null,
+	rep_com varchar2(800),
+	rep_hide number(1) default 0 not null,
+	rep_status number(1) default 0 not null,
+	rep_regdate date default sysdate not null,
+	com_num number not null,
+	report_mem number not null,
+	reported_mem number not null,
+	constraint report_com_pk primary key (rcom_num),
+	constraint report_com_fk1 foreign key (com_num) references style_comment (com_num) ON DELETE CASCADE,
+	constraint report_com_fk2 foreign key (report_mem) references member (mem_num) ON DELETE CASCADE,
+	constraint report_com_fk3 foreign key (reported_mem) references member (mem_num) ON DELETE CASCADE
+);
+
+CREATE sequence report_com_seq;
