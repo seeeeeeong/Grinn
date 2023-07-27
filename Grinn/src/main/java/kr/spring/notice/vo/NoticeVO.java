e8f0fbc16816b050ab7e72b7ba57bb237b96fb50
myYeom <myYeom@BOOK-191PIMOOTS>
2023-07-27 15:59:30 +0900

    feat notice

---
+++ Grinn/src/main/java/kr/spring/notice/vo/NoticeVO.java
@@ -0,0 +1,21 @@
+package kr.spring.notice.vo;
+
+import java.sql.Date;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class NoticeVO {
+	private int no_num;
+	private int MEM_NUM;
+	private String no_title;
+	private String not_content;
+	private Date no_regdate;
+	private Date no_modifydate;
+	private int no_status;
+	private int no_category;
+}
