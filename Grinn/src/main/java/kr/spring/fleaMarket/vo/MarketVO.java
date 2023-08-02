b0cc9992e352d18e12fad7bf6e115ad08bcee05d
Honeyoatmeal <toitui64@gmail.com>
2023-08-02 14:41:58 +0900

    feat: boothList

--- Grinn/src/main/java/kr/spring/fleaMarket/vo/MarketVO.java
+++ Grinn/src/main/java/kr/spring/fleaMarket/vo/MarketVO.java
@@ -4,6 +4,7 @@ import java.sql.Date;
 
 import javax.validation.constraints.NotEmpty;
 
+import org.hibernate.validator.constraints.Range;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Getter;
@@ -25,8 +26,11 @@ public class MarketVO {
 	@NotEmpty
 	private String place_name;
 	private int market_type;
+	@Range(min=1,max=999)
 	private int booth_count;
+	@Range(min=1,max=99)
 	private int user_count;
+	@Range(min=0,max=999999)
 	private int booth_fee;
 	private byte[] market_poster;
 	private String market_posterName;
