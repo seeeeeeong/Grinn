cc5363c740aa5c1ea9e549a1c9ecc2173afc76c5
yijunesok <yijunesok@localhost>
2023-08-10 14:43:20 +0900

    복구

--- Grinn/src/main/java/kr/spring/report/service/ReportServiceImpl.java
+++ Grinn/src/main/java/kr/spring/report/service/ReportServiceImpl.java
@@ -1,10 +1,14 @@
 package kr.spring.report.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import kr.spring.report.dao.ReportMapper;
+
 @Service
 @Transactional
-public class ReportServiceImpl {
-
+public class ReportServiceImpl implements ReportService{
+	@Autowired
+	ReportMapper reportMapper;
 }
